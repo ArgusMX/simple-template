0f6fe42c63f1739f0d51ee4e726e1b5c5045ed37
Volodymyr Sokolov <sokolmx@gmail.com>
2022-10-25 12:44:54 +0300

    fix formating

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,44 @@
-(function (time){
+(function (time) {
+  const container = document.querySelector("#carousel");
+  const slides = container.querySelectorAll(".carousel-slide");
+  const prevBtn = container.querySelector("#prev");
+  const nextBtn = container.querySelector("#next");
+  const menuBtn = document.querySelector(".menu__btn");
+  const menu = document.querySelector(".menu__list");
 
-  const container = document.querySelector('#carousel');
-  const slides = container.querySelectorAll('.carousel-slide');
-  const prevBtn = container.querySelector('#prev');
-  const nextBtn = container.querySelector('#next');
-  const menuBtn = document.querySelector('.menu__btn');
-  const menu = document.querySelector('.menu__list');
-  
-  const SLIDES_COUNT = slides.length; 
+  const SLIDES_COUNT = slides.length;
   let currentSlide = 0;
   let timerID = null;
   let interval = time;
-  
-  function gotoNth(n){
-    slides[currentSlide].classList.toggle('active');
+
+  function gotoNth(n) {
+    slides[currentSlide].classList.toggle("active");
     currentSlide = (n + SLIDES_COUNT) % SLIDES_COUNT;
-    slides[currentSlide].classList.toggle('active');
+    slides[currentSlide].classList.toggle("active");
   }
-  
+
   function gotoPrev() {
     gotoNth(currentSlide - 1);
   }
-  
+
   function gotoNext() {
     gotoNth(currentSlide + 1);
   }
-  
+
   function prev() {
     gotoPrev();
   }
-  
+
   function next() {
     gotoNext();
   }
-  
+
   function initListeners() {
-    nextBtn.addEventListener('click', next);
-    prevBtn.addEventListener('click', prev);
-    menuBtn.addEventListener('click', () => {
-      menuBtn.classList.toggle('active');
-      menu.classList.toggle('active');
+    nextBtn.addEventListener("click", next);
+    prevBtn.addEventListener("click", prev);
+    menuBtn.addEventListener("click", () => {
+      menuBtn.classList.toggle("active");
+      menu.classList.toggle("active");
     });
   }
 
@@ -49,5 +48,4 @@
   }
 
   init();
-
-}(5000));
+})(5000);
